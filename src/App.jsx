cd93014c3d047efd64bec1a4038b33555d16be63
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-07-19 01:25:42 +0500

    Initialized Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./Components/Hero/Hero";
+
+const App = () => {
+  return (
+    <div className="overflow-x-hidden">
+      <Hero/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Assets/Contants.js
@@ -0,0 +1,18 @@
+import bgImg from './bg-slate.png';
+import BlackCoffee from './black.png';
+import Cover from './coffee-cover.jpg';
+import Map from './world-map.png';
+
+export const bgImage = {
+    backgroundImage: `url(${bgImg})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+}
+
+export const Image = {
+    bgImage: bgImage,
+    BlackCoffee : BlackCoffee,
+    Cover: Cover,
+    Map: Map,
+}
